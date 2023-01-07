8dc63ff8a383c59d258de8283d9904e6155e2368
novsiann <nikitaovsyannik77@gmail.com>
2023-01-07 23:18:49 +0100

    putendl_fd finished

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: novsiann <novsiann@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nikitos <nikitos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 17:07:32 by novsiann          #+#    #+#             */
-/*   Updated: 2023/01/02 17:09:27 by novsiann         ###   ########.fr       */
+/*   Updated: 2023/01/07 12:18:48 by nikitos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
